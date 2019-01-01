6429c1df328b3f5352a4ea860b56af66a14a0379
Nauman Bajwa <naumanbajwa@sdlt8970389-2.local>
2019-01-01 12:43:51 -0800

    after arranging the code

--- TDL/controllers/ViewController.swift
+++ TDL/controllers/ViewController.swift
@@ -10,33 +10,33 @@ import UIKit
 
 class ViewController: UITableViewController {
     
-   // var itemArray = ["Pray","Study","Gym"]
+    // var itemArray = ["Pray","Study","Gym"]
     var itemArray   = [Item]()
     let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
-     //   if let items = defaults.array(forKey: "ToDoListArray") as? [String]{
-     //      itemArray = items}
-       let newItem = Item()
+        if let items = defaults.array(forKey: "ToDoListArray") as? [Item]{
+        itemArray = items}
+        let newItem = Item()
         newItem.title = "Prayers"
         itemArray.append(newItem)
-        
         let newItem2 = Item()
         newItem2.title = "Studies"
         itemArray.append(newItem2)
-        
         let newItem3 = Item()
         newItem3.title = "IOS"
         itemArray.append(newItem3)
-        
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
     // MARK- tableview data course methods
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
+    
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "toDoItemCell", for: indexPath)
         let item = itemArray[indexPath.row]
@@ -44,49 +44,40 @@ class ViewController: UITableViewController {
         cell.accessoryType = item.done ? .checkmark : .none
         return cell
     }
+    
+    
     // MARK - table view delegate method
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print(itemArray[indexPath.row])
-        
-      // to change the check property of the item
-     itemArray[indexPath.row].done = !itemArray[indexPath.row].done
-        
+        // to change the check property of the item
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         tableView.reloadData()
-       tableView.deselectRow(at: indexPath, animated: true)
+        tableView.deselectRow(at: indexPath, animated: true)
     }
-    //Mark - Add new items
     
+    
+    //Mark - Add new items
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
-        
         var textField = UITextField()
-        
         let alert = UIAlertController(title: "To do List Item", message: "", preferredStyle: .alert)
-        
         let action = UIAlertAction(title: "Add item", style: .default){(action) in
             // what will happen if the user clicks add item button on UI alert
-           // print("Success!")
-           // print(textField.text!)
+            // print("Success!")
+            // print(textField.text!)
             if textField.text != ""{
-                
-            
-            let newItem   = Item()
-            newItem.title = textField.text!
-            self.itemArray.append(newItem)
-            self.defaults.set(self.itemArray, forKey: "ToDoListArray")
-            self.tableView.reloadData()
+                let newItem   = Item()
+                newItem.title = textField.text!
+                self.itemArray.append(newItem)
+                self.defaults.set(self.itemArray, forKey: "ToDoListArray")
+                self.tableView.reloadData()
             }
-            
         }
-            
-            alert.addTextField{ (alertTextField) in
-                alertTextField.placeholder = "Create New Item"
-               
-                textField = alertTextField
-            }
-        
+        alert.addTextField{ (alertTextField) in
+            alertTextField.placeholder = "Create New Item"
+            textField = alertTextField
+        }
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
-        
     }
 }
 
