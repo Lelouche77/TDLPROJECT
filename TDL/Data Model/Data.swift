fbb1cf54bde373d0abe25be3dade68a6e4aba591
Nauman Bajwa <naumanbajwa@sdlt8970389-2.local>
2019-01-05 18:45:46 -0800

    saving first data using realm

---
+++ TDL/Data Model/Data.swift
@@ -0,0 +1,16 @@
+//
+//  Data.swift
+//  TDL
+//
+//  Created by Nauman Bajwa on 1/5/19.
+//  Copyright © 2019 Nauman Bajwa. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Data: Object {
+    @objc dynamic var name : String = ""
+    @objc dynamic var age : Int     = 0
+    
+}
