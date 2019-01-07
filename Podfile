a498f779f1f74f0e4d0e758daeca5311feda591b
Nauman Bajwa <naumanbajwa@sdlt8970389-2.local>
2019-01-06 23:25:37 -0800

    REALm crud + quarry

---
+++ Podfile
@@ -0,0 +1,9 @@
+ platform :ios, '9.0'
+
+target 'TDL' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+pod 'RealmSwift'
+  # Pods for TDL
+
+end
--- TDL/controllers/ViewController.swift
+++ TDL/controllers/ViewController.swift
@@ -76,6 +76,7 @@ class ViewController: UITableViewController {
                     try self.realm.write {
                         let newItem = Item()
                         newItem.title = textField.text!
+                        newItem.dateCreated = Date()
                         currentCategory.items.append(newItem)
                     }
                 }catch {
@@ -101,28 +102,20 @@ class ViewController: UITableViewController {
     }
     
     
-}/*
- // Mark: search bar methods
- extension ViewController: UISearchBarDelegate {
- func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
- //    let request : NSFetchRequest<Item> = Item.fetchRequest()
- 
- //     let categoryPredicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
- //     let compoundPredicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categoryPredicate, predicate ])
- //     request.predicate = compoundPredicate
- 
- //     let predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
- //    request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
- //   loadItems(with: request, predicate: predicate)
- 
- }
- func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
- if searchBar.text?.count == 0 {
- //       loadItems()
- DispatchQueue.main.async {
- searchBar.resignFirstResponder()
- }
- 
- }
- }/
- }*/
+}
+// Mark: search bar methods
+extension ViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+     toDoItems = toDoItems?.filter("title CONTAINS[cd] %@ ", searchBar.text!).sorted(byKeyPath: "dateCreated", ascending: true)
+        
+    }
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+                loadItems()
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+            
+        }
+    }
+}
